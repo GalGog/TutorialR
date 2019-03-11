8fd67d5b791b16168e7d0005bfca0aa9c2324b3e
GalGog <galynna00@gmail.com>
2019-03-11 18:47:19 +0000

    passed a prop from a parent Board component to a child Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,8 @@ import React, { Component} from 'react';
 class Square extends Component {
     render(){
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={ ()=> alert('click')}>
+            {this.props.value}
             </button>
         );
     }
